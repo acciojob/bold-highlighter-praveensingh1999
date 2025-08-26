82b9b339d9e2097a2469da33005ede6af38ebb11
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-08-26 23:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
+	let highlight = document.querySelectorAll("strong");
+	highlight.style.color="rgb(0, 128, 0)";
+ 
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+let highlight = document.querySelectorAll("strong");
+	highlight.style.color="rgb(0, 0, 0)";
     
 }
